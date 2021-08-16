1082073fff652111bbc7ecc6d8008d48d613321f
11SF <nawaphong.sit@dome.tu.ac.th>
2021-08-16 15:28:31 +0700

    edit menu icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import './Navbar.css'
 
-import hamberger from '../../asset/hamburger_icon.svg'
+// import hamberger from '../../asset/hamburger_icon.svg'
 // import top_wave from '../../asset/wave.svg';
 
 export default function Navbar({isAuth}) {
@@ -18,15 +18,15 @@ export default function Navbar({isAuth}) {
                     <p className="logo" id="nav_logo" >ถึงเวลาจ่ายค่า Spotify รึยังน้าาา</p>  
                     { isAuth ? <Link to="/admin" className="header_btn" ><p>ข้อมูลสมาชิก</p></Link>
                         : <Link to="/login" className="header_btn" ><p>เข้าสู่ระบบ</p></Link>}  
-                    <img className="hamberger" src={hamberger} alt="hamberger" onClick={()=> dropMenu()}></img>
+                    <svg onClick={()=> dropMenu()} xmlns="http://www.w3.org/2000/svg" className="hamberger h-10 w-10" fill="#f4f4f4" viewBox="0 0 24 24" stroke={toggle ? "#000000" : "#f4f4f4"}>
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                    {/* <img className="hamberger" src={hamberger} alt="hamberger" onClick={()=> dropMenu()}></img> */}
                 </div>
             </div> 
             <div className={toggle ? "dropMenu" : "dropMenu hidden"}>
                 <ul>
-                    <li className="menu_item">เข้าสู่ระบบ</li>
-                    <li className="menu_item">เข้าสู่ระบบ</li>
-                    <li className="menu_item">เข้าสู่ระบบ</li>
-                    <li className="menu_item">เข้าสู่ระบบ</li>
+                    <li className="menu_item"><Link to="/login">เข้าสู่ระบบ</Link></li>
                 </ul>
             </div>
             <div className="top_wave"></div>
